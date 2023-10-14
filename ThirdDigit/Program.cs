bde1cf7230857f1a46e0a3c3d21a64b9644b1042
Dmitry Kolomytsev <docdmk@icloud.com>
2023-10-15 00:12:57 +0600

    first coomit

---
+++ ThirdDigit/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+    Console.WriteLine("Третьего числа нет!");
+else
+{
+    while (number > 1000)
+        number = number / 10;
+    Console.WriteLine("Третье число равно " + number % 10);
+}
