9206ea972ed46876ca6ab4efe9c96861769cd948
Dmitry Kolomytsev <docdmk@icloud.com>
2023-10-15 00:18:55 +0600

    Hometask ready

---
+++ Day/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 7 || day < 1)
+Console.WriteLine("Такого дня недели нет!");
+else if (day == 6 || day == 7) 
+Console.WriteLine("Это выходной");
+else
+Console.WriteLine("Это не выходной");
---
+++ SecondDigit/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+string numb = Console.ReadLine();
+int number = Convert.ToInt32(numb);
+int result = (number / 10) % 10;
+Console.WriteLine($"Вторая цифра — {result}");
